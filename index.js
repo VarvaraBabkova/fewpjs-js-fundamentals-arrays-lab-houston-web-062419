1d840e1aa3bade5113e5b13cf7b4e0addb0a9674
Varvara Babkova <varvara.munday@gmail.com>
2019-08-05 19:55:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ const prepend = ["Milo", "Otis", "Garfield"];
 const removeLast = ["Milo", "Otis", "Garfield"];
 const removeFirst = ["Milo", "Otis", "Garfield"];
 
-append.push('Odie')
+append.push('Odie');
+prepend.unshift('Odie');
